f68bd40326a736b4b482fc13ded0733249dcedd4
JSpiner <jspiner@naver.com>
2016-09-30 17:15:09 +0000

    add gitignore, setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+setup(
+   name="SlackApi",
+   version='0.1.1',
+   py_modules=['slackapi'],
+   author='JSpiner',
+   author_email='jspiner@naver.com',
+   url='https://github.com/JSpiner/SlackApi',
+   description="Python Simple Slack Event Api",
+)
